c8d512025feba6b9ac14b0bfdf637815d29885f2
comment <obrazovsergey@gmail.com>
2023-05-11 15:52:06 +0300

    employees

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {useState, useEffect} from "react";
 
 const Customers = () => {
+
+    const [customers, setCustomers] = useState([])
+
+    useEffect(() => {
+        console.log('fetching...');
+            fetch('http://localhost:8000/api/customers/')
+            .then((response) => response.json())
+            .then((data) => {
+                setCustomers(data.customers);
+                console.log(data);
+            })
+    }, [])
+
     return (
         <div>
-            <h1>Hello world!</h1>
+            <h1>Customers from api</h1>
+            {customers ? customers.map((customer) => {
+                return <p>{customer.name}</p>
+            }) : null}
         </div>
     );
 };

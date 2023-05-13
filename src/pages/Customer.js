42613845cad22c8a7bcd386200de13fa0170b592
Sergey <obrazcovsergey@gmail.com>
2023-05-13 10:14:40 +0300

    fix left button

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { baseUrl } from "../shared";
-import Button from 'react-bootstrap/Button';
 
 const Customer = () => {
 

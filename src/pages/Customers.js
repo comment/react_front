a9fd041c8f94460630c2a8e4a647dc64894f036b
Sergey <obrazcovsergey@gmail.com>
2023-05-13 10:11:59 +0300

    add styles and save by enter, edit inputs

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState, useEffect} from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { baseUrl } from "../shared";
 import AddCostumer from "../components/AddCostumer";
 
